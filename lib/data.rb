5b40efafc1241125ccaaa716bcda675b6d121a65
Ryan Taylor <ryanmt@byu.net>
2011-06-10 13:49:20 -0600

    It works, and has some example output files

---
+++ lib/data.rb
@@ -0,0 +1,162 @@
+module Ms
+	def precursor_mass(seq, charge)
+		mass = NTerm + CTerm
+		seq.chars.map(&:to_sym).each do |residue|
+			mass += Ms::ResidueMasses[residue]
+		end
+		charge_state(mass, charge)
+	end
+		
+	def charge_state(mass, charge)
+		if charge > 0
+			(mass + charge) / charge.to_f
+		else
+			(mass - charge) / charge.to_f
+		end
+	end
+
+	IonTypeMassDelta = {
+		a:  (- 29.00273),
+		a_star: (-(29.00273+17.02654)),
+		a_not:	(-(17.02654 + 29.00273+18.01056)),
+		b: (-1.00782),
+		b_star: ( - 1.00782 - 17.02654),
+		b_not: (-17.02654 - 1.00782 - 18.01056),
+		c: (16.01872),
+		x: (27.99491 - 1.00782),
+		y: (1.00782),
+		y_star: (1.00782  - 17.02654),
+		y_not: (1.00782 - 18.01056),
+		z: (- 16.01872)
+	}
+	
+	NTerm =	1.00782
+
+	CTerm =	27.99491 - 10.9742
+
+	ResidueMasses = {
+		:A => 71.037114, 
+		:R => 156.101111,
+		:N => 114.042927,
+		:D => 115.026943,
+		:C => 103.009185,
+		:E => 129.042593,
+		:Q => 128.058578, 
+		:G => 57.021464,
+		:H => 137.058912,
+		:I => 113.084064,
+		:L => 113.084064,
+		:K => 128.094963,
+		:M => 131.040485,
+		:F => 147.068414,
+		:P => 97.052764,
+		:S => 87.032028,
+		:T => 101.047679,
+		:U => 150.95363,
+		:W => 186.079313,
+		:Y => 163.06332,
+		:V => 99.068414
+	}
+################
+# This is straight from my pI calculator, and adds the fxn of calculating a maximum charge state for the total peptide, given the sequence.
+#
+#	Usage:  charge_at_pH(identify_potential_charges(peptide_sequence), pH_desired)
+	PepCharges = Struct.new(:seq, :n_term, :c_term, :y_num, :c_num, :k_num, :h_num, :r_num, :d_num, :e_num, :pi)
+	def identify_potential_charges(str)
+		string = str.upcase
+		first = string[0]; last = string[-1]
+		puts string if first.nil? or last.nil?
+		begin
+			out = PepCharges.new(string, Table[first.to_sym][0], Table[last.to_sym][1], 0, 0, 0 ,0 ,0 ,0, 0)
+		rescue NoMethodError
+			abort string
+		end
+		string.chars.each do |letter|
+			case letter
+				when "Y" 
+					out.y_num += 1 
+				when "C"
+					out.c_num += 1
+				when "K"
+					out.k_num += 1 
+				when "H"
+					out.h_num += 1
+				when "R"
+					out.r_num += 1
+				when "D"
+					out.d_num += 1
+				when "E"
+					out.e_num += 1
+			end
+		end
+		out
+	end # Returns the PepCharges structure
+
+	Table = {
+		:K => [2.18,8.95,10.53], 
+		:E => [2.19,9.67,4.25], 
+		:D => [1.88,9.60,3.65], 
+		:H => [1.82,9.17,6.00],
+		:R => [2.17,9.04,12.48],
+		:Q => [2.17,9.13,nil],
+		:N => [2.02,8.80,nil],
+		:C => [1.96,10.28,8.18],
+		:T => [2.11,9.62,nil],
+		:S => [2.21,9.15,nil],
+		:W => [2.38,9.39,nil],
+		:Y => [2.20,9.11,10.07],
+		:F => [1.83,9.13,nil],
+		:M => [2.28,9.21,nil],
+		:I => [2.36,9.68,nil],
+		:L => [2.36,9.60,nil],
+		:V => [2.32,9.62,nil],
+		:P => [1.99,10.96,nil],
+		:A => [2.34,9.69,nil],
+		:G => [2.34,9.60,nil],
+# These are the fringe cases... B and Z... Jerks, these are harder to calculate pIs
+		:B => [1.95,9.20,3.65],
+		:Z => [2.18,9.40,4.25],
+		:X => [2.20,9.40,nil],
+		:U => [1.96,10.28,5.20] # Unfortunately, I've only found the pKr for this... so I've used Cysteine's values.
+	}
+
+	def charge_at_pH(pep_charges, pH)
+		charge = 0
+		charge += -1/(1+10**(pep_charges.c_term-pH))
+		charge += -pep_charges.d_num/(1+10**(Table[:D][2]-pH))
+		charge += -pep_charges.e_num/(1+10**(Table[:E][2]-pH))
+		charge += -pep_charges.c_num/(1+10**(Table[:C][2]-pH))
+		charge += -pep_charges.y_num/(1+10**(Table[:Y][2]-pH))
+		charge += 1/(1+10**(pH - pep_charges.n_term))
+		charge += pep_charges.h_num/(1+10**(pH-Table[:H][2]))
+		charge += pep_charges.k_num/(1+10**(pH-Table[:K][2]))
+		charge += pep_charges.r_num/(1+10**(pH-Table[:R][2]))
+		charge
+	end
+############################################################
+end
+=begin
+Formula: H3N1
+
+Monoisotopic mass :     17.02654
+
+Formula: C1H1O1
+
+Monoisotopic mass :     29.00273
+
+Formula: H2O1
+
+Monoisotopic mass :     18.01056
+
+Formula: H1
+
+Monoisotopic mass :      1.00782
+
+Formula: H2N1
+
+Monoisotopic mass :     16.01872
+
+Formula: C1O1
+
+Monoisotopic mass :     27.99491
+=end
---
+++ lib/fragmenter.rb
@@ -0,0 +1,131 @@
+require 'data'
+class Fragmenter
+	include Ms
+
+	attr_accessor :list
+	TableEntry = Struct.new(:ion, :seq, :mass, :charge)
+	Default_fragments = {:b => true, :y => true}
+	def initialize(opts= {})
+		@opts = Default_fragments.merge(opts)
+		@n_term_search_ion_types = []
+		@c_term_search_ion_types = []
+		opts = Default_fragments.merge(opts)
+		opts.each do |key, v|
+			if v
+				case key 
+					when :b
+						@n_term_search_ion_types.push(:b, :b_star, :b_not)
+					when :a
+						@n_term_search_ion_types.push(:a, :a_star, :a_not)
+					when :c
+						@n_term_search_ion_types << :c
+					when :x
+						@c_term_search_ion_types << :x
+					when :y
+						@c_term_search_ion_types.push(:y, :y_star, :y_not)
+					when :z
+						@c_term_search_ion_types << :z
+				end
+			end
+		end
+	end #initialize
+		
+	def calculate_fragments(sequence)
+		arr = sequence.upcase.split('')
+		out = [[],[]]
+		(0..arr.size-2).each do |i|
+			out[0] << arr[0..i].join
+			out[1] <<  arr[(i+1)..-1].join
+		end
+		out
+	end
+	def generate_fragment_masses(sequence) # Returns the TableEntry object which should be easy to use for table generation
+		@sequence = sequence
+		@max_charge = charge_at_pH(identify_potential_charges(sequence), 2).round
+		### Calculate the base ion masses	
+		n_terms, c_terms = calculate_fragments(sequence)
+		n_terms.map! do |seq|
+			mass = Ms::NTerm
+			seq.chars.map(&:to_sym).each do |residue|
+				mass += Ms::ResidueMasses[residue]
+			end
+			[seq, mass]
+		end
+		c_terms.map! do |seq|
+			mass = Ms::CTerm
+			seq.chars.map(&:to_sym).each do |residue|
+				mass += Ms::ResidueMasses[residue]
+			end
+			[seq, mass]
+		end
+### Tablify and generate a comprehensive list of ions
+		list = []
+		send_to_list = lambda do |fragment_arr, iontypes_arr|
+			fragment_arr.each do |n_terms|
+				seq = n_terms.first
+				mass = n_terms.last
+				iontypes_arr.each do |iontype|
+					(1..@max_charge).each do |charge|
+						charge_legend = '+'*charge
+						list << TableEntry.new("#{iontype}(#{seq.size})#{charge_legend}".to_sym, seq, charge_state(mass + IonTypeMassDelta[iontype], charge), charge)
+					end # 1..max_charge
+				end	# iontypes_arr
+			end # fragment_arr
+		end # lambda block
+		send_to_list.call(n_terms, @n_term_search_ion_types)
+		send_to_list.call(c_terms, @c_term_search_ion_types)		
+		@list = list
+	end	
+	def to_mgf(seq = nil)
+		if seq.nil?
+			seq = @sequence
+			list = @list
+		else
+			list = generate_fragment_masses(seq)
+		end
+		intensity = 1000 # An arbitrary intensity value
+		output_arr = []
+		output_arr << %Q{COM=Project: In-silico Fragmenter\nBEGIN IONS\nPEPMASS=#{precursor_mass(seq, @max_charge)}\nCHARGE=#{@max_charge}+\nTITLE=Label: Sequence is #{seq}}
+		list.sort_by{|a| a.mass}.each do |table_entry|
+			#	TableEntry = Struct.new(:ion, :seq, :mass, :charge)
+			output_arr << "#{"%.5f" % table_entry.mass }\t#{intensity}"
+		end
+		output_arr << "END IONS"
+		output_arr.join("\n")
+	end
+	def graph(list = nil)
+		list ? list : list = @list
+		require 'rserve/simpler'
+		robj = Rserve::Simpler.new
+		hash = {}
+		hash["mass"] = list.map{|a| a.mass}
+		hash["intensity"] = list.map{ 1000}
+		robj.converse( masses: hash.to_dataframe) do 
+			"attach(masses)"
+		end
+		#robj.converse( data: Rserve::DataFrame.from_structs(list))
+		robj.converse do 
+			%Q{png(file='/home/ryanmt/Dropbox/coding/bootcamp/fragmenter/#{@sequence}_spectra.png')
+				plot(masses$mass, masses$intensity, type='h')
+				dev.off()
+		}
+		end	
+	end
+		
+end
+ 
+######### Testing stuff
+
+test = "HELL"
+test2= "AALK"
+
+f = Fragmenter.new
+f.generate_fragment_masses(test2)
+puts f.to_mgf
+
+puts f.to_mgf(test2)
+puts f.to_mgf("VFSNGADLSGVTEEAPLK")
+p f.list
+
+
+
